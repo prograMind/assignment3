d327012bf8c6718330554de9ff2cd1d4702ee911
prograMind <jan.kriz@mail.com>
2022-03-10 15:19:03 +0100

    deleted renders in index.js

--- assignment3/pages/index.js
+++ assignment3/pages/index.js
@@ -11,18 +11,7 @@ export default function Home() {
         <h1>My Startup Progress</h1>
       </main>
       <div>
-        <Stage name="Foundation">
-          <Item item="Create roadmap"></Item>
-          <Item item="Competitor analysis"></Item>
-        </Stage>
-        <Stage name="Discovery">
-          <Item item="Create roadmap"></Item>
-          <Item item="Competitor analysis"></Item>
-        </Stage>
-        <Stage name="Delivery">
-          <Item item="Setup virtual office"></Item>
-          <Item item="Setup virtual office"></Item>
-        </Stage>
+        <Stage />
       </div>
     </div>
   )
